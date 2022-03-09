5a44294c63a273ff3cc435fd525b1cc9b0409efe
luoChen <hank.luo@xianghuanji.com>
2022-03-09 15:33:42 +0800

    readme

--- javaJvm/src/test/java/com/niluogege/javajvm/HelloWorld.java
+++ javaJvm/src/test/java/com/niluogege/javajvm/HelloWorld.java
@@ -2,8 +2,8 @@ package com.niluogege.javajvm;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 1; i <= 10; i++) {
+        int sum = 1;
+        for (int i = 2; i <= 3; i++) {
             sum += i;
         }
         System.out.println(sum);
