34ad905bc4464dd9984c17765fbc68942c48d012
luoChen <hank.luo@xianghuanji.com>
2022-03-14 14:14:11 +0800

    调试

--- javaJvm/src/main/java/com/niluogege/javajvm/Interpret.java
+++ javaJvm/src/main/java/com/niluogege/javajvm/Interpret.java
@@ -1,5 +1,6 @@
 package com.niluogege.javajvm;
 
+import com.alibaba.fastjson.JSON;
 import com.niluogege.javajvm.instructions.Factory;
 import com.niluogege.javajvm.instructions.base.BytecodeReader;
 import com.niluogege.javajvm.instructions.base.Instruction;
@@ -16,7 +17,7 @@ public class Interpret {
         Thread thread = new Thread();
         Frame frame = thread.newFrame(method);
         thread.pushFrame(frame);
-        loop(thread,method.code);
+        loop(thread, method.code);
     }
 
     private void loop(Thread thread, byte[] byteCode) {
@@ -25,13 +26,13 @@ public class Interpret {
 
         BytecodeReader reader = new BytecodeReader();
 
-        while (true){
+        while (true) {
             //获取下一个指定
             int pc = frame.nextPc();
             //pc 记录到 Thread
             thread.setPc(pc);
             //重置reader
-            reader.reset(byteCode,pc);
+            reader.reset(byteCode, pc);
             //读取操作指令代码
             byte opcode = reader.readByte();
             //将 指令代码 转换为 真正的指令
@@ -41,8 +42,16 @@ public class Interpret {
                 break;
             }
 
-            System.out.println("inst="+inst);
+            //获取操作数
+            inst.fetchOperands(reader);
 
+            //设置下一个pc寄存器
+            frame.setNextPc(reader.getPc());
+
+            //执行
+            inst.execute(frame);
+
+            System.out.format("pc寄存器值 ：0x%x 对应的指令 %s => 当前Frame的局部变量表：%s 当前Frame的操作数栈：%s\n", opcode, inst.getClass().getSimpleName(), JSON.toJSONString(frame.localVars().getSlots()), JSON.toJSONString(frame.operandStack().getSlots()));
         }
 
     }
